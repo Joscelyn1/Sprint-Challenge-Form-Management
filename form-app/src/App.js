a707fd01e16eddb429257b412fafebc67e75ea1f
Joscelyn <JoscelynRose@gmail.com>
2019-08-09 12:58:08 -0400

    made class components

--- form-app/src/App.js
+++ form-app/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import FormComponent from "./components/FormComponent.js";
 import "./App.css";
+import Header from "./components/Header.js";
 
-function App() {
-  return (
-    <div className="App">
-      <FormComponent />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <FormComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
